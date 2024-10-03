ea4c6490a8c120e7aff3829c742ab90d751c364b
João Vitor Zanardo <100529680+joaovitorzanardo@users.noreply.github.com>
2024-10-03 19:24:45 -0300

    Initial commit

---
+++ qualuga/src/test/java/com/uri/qualuga/QualugaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uri.qualuga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualugaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
